7e190052270b9cbcf2c636d9366e8bb1ef95bc70
moshaad7 <65341373+moshaad7@users.noreply.github.com>
2021-06-26 21:21:33 +0530

    sort updated

--- src/components/main/sort.jsx
+++ src/components/main/sort.jsx
@@ -1,81 +1,115 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react'
+import { add, del, mod } from '../../reduxTools/actions'
+import { connect } from 'react-redux'
 class Sort extends Component {
   state = {
-    field: "", // new field to sort on
-    sortOrder: "asc", // sort order
+    field: '', // new field to sort on
+    sortOrder: 'asc', // sort order
     sortingScheme: [], // all fields and their sort order
-    heirarchy: ["Sort"],
-  };
+    heirarchy: this.props.heirarchy,
+  }
+
+  componentWillUnmount() {
+    let newSort = {
+      sort: [],
+    }
+
+    this.state.sortingScheme.forEach((scheme) => {
+      newSort.sort.push({
+        [scheme.field]: {
+          order: [scheme.sortOrder],
+        },
+      })
+    })
+    this.props.DELETE(this.props.heirarchy, newSort)
+  }
+
+  componentDidUpdate = (prevProps, prevState, snapshot) => {
+    if (this.state.sortingScheme.length !== prevState.sortingScheme.length) {
+      let newSort = {
+        sort: [],
+      }
+
+      this.state.sortingScheme.forEach((scheme) => {
+        newSort.sort.push({
+          [scheme.field]: {
+            order: [scheme.sortOrder],
+          },
+        })
+      })
+
+      this.props.ADD(this.props.heirarchy, newSort)
+    }
+  }
 
   updateSortingScheme = () => {
-    let temp = this.state.sortingScheme;
-    let newID;
+    let temp = this.state.sortingScheme
+    let newID
     try {
-      newID = temp[temp.length - 1]["id"] + 1;
+      newID = temp[temp.length - 1]['id'] + 1
     } catch {
-      newID = 0;
+      newID = 0
     }
     let newSort = {
       id: newID,
       field: this.state.field,
       sortOrder: this.state.sortOrder,
-    };
+    }
     this.setState({
       sortingScheme: [...temp, newSort],
-      field: "",
-      sortOrder: "asc",
-    });
-  };
+      field: '',
+      sortOrder: 'asc',
+    })
+  }
 
   deleteItem = (id) => {
     this.setState({
-      sortingScheme: this.state.sortingScheme.filter((a) => a["id"] !== id),
-    });
-  };
+      sortingScheme: this.state.sortingScheme.filter((a) => a['id'] !== id),
+    })
+  }
 
   render() {
     return (
-      <div className="card">
-        <div className="card-header d-flex justify-content-between  align-items-center">
+      <div className='card'>
+        <div className='card-header d-flex justify-content-between  align-items-center'>
           Sort
           <button
             onClick={(a) => this.props.onDelete(this.props.element)}
-            type="button"
-            className="btn btn-sm btn-danger"
+            type='button'
+            className='btn btn-sm btn-danger'
           >
             Delete
           </button>
         </div>
-        <div className="card-body">
+        <div className='card-body'>
           {this.state.sortingScheme.length !== 0 ? (
-            <table class="table">
+            <table class='table'>
               <thead>
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Field</th>
-                  <th scope="col">Order</th>
-                  <th scope="col">Action</th>
+                  <th scope='col'>#</th>
+                  <th scope='col'>Field</th>
+                  <th scope='col'>Order</th>
+                  <th scope='col'>Action</th>
                 </tr>
               </thead>
               <tbody>
                 {this.state.sortingScheme.map((scheme) => {
                   return (
                     <tr>
-                      <th scope="row">{scheme.id}</th>
+                      <th scope='row'>{scheme.id}</th>
                       <td>{scheme.field}</td>
                       <td>{scheme.sortOrder}</td>
                       <td>
                         <button
-                          type="button"
+                          type='button'
                           onClick={() => this.deleteItem(scheme.id)}
-                          class="btn btn-sm btn-danger"
+                          class='btn btn-sm btn-danger'
                         >
                           Delete
                         </button>
                       </td>
                     </tr>
-                  );
+                  )
                 })}
               </tbody>
             </table>
@@ -83,12 +117,12 @@ class Sort extends Component {
             <React.Fragment />
           )}
 
-          <div className="form-floating mb-3">
+          <div className='form-floating mb-3'>
             <input
-              type="text"
-              className="form-control"
-              id="sortField"
-              placeholder="Field Name"
+              type='text'
+              className='form-control'
+              id='sortField'
+              placeholder='Field Name'
               value={this.state.field}
               onChange={(e) =>
                 this.setState({
@@ -96,54 +130,68 @@ class Sort extends Component {
                 })
               }
             />
-            <label for="sortField">Field Name</label>
+            <label for='sortField'>Field Name</label>
           </div>
 
-          <div className="form-check form-check-inline">
+          <div className='form-check form-check-inline'>
             <input
-              className="form-check-input"
-              type="radio"
-              name="sortOrder"
-              id="asc"
-              value="asc"
+              className='form-check-input'
+              type='radio'
+              name='sortOrder'
+              id='asc'
+              value='asc'
               onClick={(e) =>
                 this.setState({
                   sortOrder: e.target.value,
                 })
               }
             />
-            <label className="form-check-label" for="asc">
+            <label className='form-check-label' for='asc'>
               Asc
             </label>
           </div>
-          <div className="form-check form-check-inline">
+          <div className='form-check form-check-inline'>
             <input
-              className="form-check-input"
-              type="radio"
-              name="sortOrder"
-              id="desc"
-              value="desc"
+              className='form-check-input'
+              type='radio'
+              name='sortOrder'
+              id='desc'
+              value='desc'
               onClick={(e) =>
                 this.setState({
                   sortOrder: e.target.value,
                 })
               }
             />
-            <label className="form-check-label" for="desc">
+            <label className='form-check-label' for='desc'>
               Desc
             </label>
           </div>
           <button
-            type="button"
-            className="btn btn-sm btn-success"
+            type='button'
+            className='btn btn-sm btn-success'
             onClick={this.updateSortingScheme}
           >
             Add New Field
           </button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Sort;
+const mapStatetoProps = (props) => {
+  return {
+    queryReducer: props.queryReducer,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    ADD: (h, obj) => dispatch(add(h, obj)),
+    DELETE: (h, obj) => dispatch(del(h, obj)),
+    MODIFY: (h, obj) => dispatch(mod(h, obj)),
+  }
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps)(Sort)
