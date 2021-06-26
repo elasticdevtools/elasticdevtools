4fbe8f84afcd06a3f16795a16934dd7d3fbbdd7d
msidrisi91 <mohdshaheem91@gmail.com>
2021-06-26 19:01:11 +0530

    sort updated

--- src/components/main/sort.jsx
+++ src/components/main/sort.jsx
@@ -1,117 +1,149 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Sort extends Component {
-    state = {
-        field: '', // new field to sort on
-        sortOrder: 'asc', // sort order
-        sortingScheme: [], // all fields and their sort order
-        heirarchy: ['Sort'],
+  state = {
+    field: "", // new field to sort on
+    sortOrder: "asc", // sort order
+    sortingScheme: [], // all fields and their sort order
+    heirarchy: ["Sort"],
+  };
+
+  updateSortingScheme = () => {
+    let temp = this.state.sortingScheme;
+    let newID;
+    try {
+      newID = temp[temp.length - 1]["id"] + 1;
+    } catch {
+      newID = 0;
     }
+    let newSort = {
+      id: newID,
+      field: this.state.field,
+      sortOrder: this.state.sortOrder,
+    };
+    this.setState({
+      sortingScheme: [...temp, newSort],
+      field: "",
+      sortOrder: "asc",
+    });
+  };
 
-    updateSortingScheme = () => {
-        let temp = this.state.sortingScheme
-        let newSort = {
-            field: this.state.field,
-            sortOrder: this.state.sortOrder,
-        }
-        this.setState({
-            sortingScheme: [...temp, newSort],
-            field: '',
-            sortOrder: 'asc',
-        })
-    }
+  deleteItem = (id) => {
+    this.setState({
+      sortingScheme: this.state.sortingScheme.filter((a) => a["id"] !== id),
+    });
+  };
 
-    render() {
-        return (
-            <div className='card'>
-                <div className='card-header d-flex justify-content-between  align-items-center'>
-                    Sort
-                    <button
-                        onClick={(a) => this.props.onDelete(this.props.element)}
-                        type='button'
-                        className='btn btn-sm btn-danger'
-                    >
-                        Delete
-                    </button>
-                </div>
-                <div className='card-body'>
-                    <div className='card'>
-                        {this.state.sortingScheme.map((scheme) => {
-                            return (
-                                <div className='card-body'>
-                                    <p>
-                                        {scheme.field}: {scheme.sortOrder}
-                                    </p>
-                                </div>
-                            )
-                        })}
-                    </div>
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header d-flex justify-content-between  align-items-center">
+          Sort
+          <button
+            onClick={(a) => this.props.onDelete(this.props.element)}
+            type="button"
+            className="btn btn-sm btn-danger"
+          >
+            Delete
+          </button>
+        </div>
+        <div className="card-body">
+          {this.state.sortingScheme.length !== 0 ? (
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Field</th>
+                  <th scope="col">Order</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.sortingScheme.map((scheme) => {
+                  return (
+                    <tr>
+                      <th scope="row">{scheme.id}</th>
+                      <td>{scheme.field}</td>
+                      <td>{scheme.sortOrder}</td>
+                      <td>
+                        <button
+                          type="button"
+                          onClick={() => this.deleteItem(scheme.id)}
+                          class="btn btn-sm btn-danger"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : (
+            <React.Fragment />
+          )}
 
-                    <div className='card'>
-                        <div className='card-body'>
-                            <div className='form-floating mb-3'>
-                                <input
-                                    type='text'
-                                    className='form-control'
-                                    id='sortField'
-                                    placeholder='Field Name'
-                                    value={this.state.field}
-                                    onChange={(e) =>
-                                        this.setState({
-                                            field: e.target.value,
-                                        })
-                                    }
-                                />
-                                <label for='sortField'>Field Name</label>
-                            </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="sortField"
+              placeholder="Field Name"
+              value={this.state.field}
+              onChange={(e) =>
+                this.setState({
+                  field: e.target.value,
+                })
+              }
+            />
+            <label for="sortField">Field Name</label>
+          </div>
 
-                            <div className='form-check form-check-inline'>
-                                <input
-                                    className='form-check-input'
-                                    type='radio'
-                                    name='sortOrder'
-                                    id='asc'
-                                    value='asc'
-                                    onClick={(e) =>
-                                        this.setState({
-                                            sortOrder: e.target.value,
-                                        })
-                                    }
-                                />
-                                <label className='form-check-label' for='asc'>
-                                    Asc
-                                </label>
-                            </div>
-                            <div className='form-check form-check-inline'>
-                                <input
-                                    className='form-check-input'
-                                    type='radio'
-                                    name='sortOrder'
-                                    id='desc'
-                                    value='desc'
-                                    onClick={(e) =>
-                                        this.setState({
-                                            sortOrder: e.target.value,
-                                        })
-                                    }
-                                />
-                                <label className='form-check-label' for='desc'>
-                                    Desc
-                                </label>
-                            </div>
-                            <button
-                                type='button'
-                                className='btn btn-sm btn-success'
-                                onClick={this.updateSortingScheme}
-                            >
-                                Add New Field
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="sortOrder"
+              id="asc"
+              value="asc"
+              onClick={(e) =>
+                this.setState({
+                  sortOrder: e.target.value,
+                })
+              }
+            />
+            <label className="form-check-label" for="asc">
+              Asc
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="sortOrder"
+              id="desc"
+              value="desc"
+              onClick={(e) =>
+                this.setState({
+                  sortOrder: e.target.value,
+                })
+              }
+            />
+            <label className="form-check-label" for="desc">
+              Desc
+            </label>
+          </div>
+          <button
+            type="button"
+            className="btn btn-sm btn-success"
+            onClick={this.updateSortingScheme}
+          >
+            Add New Field
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Sort
+export default Sort;
--- src/components/query/bool.jsx
+++ src/components/query/bool.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-import Match from "./match";
+import GetElementByName from "../../utils/getelementbyname";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Bool extends Component {
+  state = {
+    options: ["Bool", "Match", "Match Phrase"],
+    must: [],
+    mustNot: [],
+    should: [],
+  };
   render() {
     return (
       <React.Fragment>
@@ -22,8 +28,39 @@ class Bool extends Component {
           ) : (
             <div className="card-header">Bool</div>
           )}
-          <div>
-            <Match heirarchy={[1, 2, 3]} />
+          <div className="card g-2">
+            <h6>Must</h6>
+            {this.state.must.map((a) => (
+              <GetElementByName
+                key={a.key}
+                heirarchy={this.state.heirarchy}
+                onDelete={this.handleDelete}
+                element={a}
+              />
+            ))}
+            <div className="input-group">
+              <select
+                className="form-select"
+                id="inputGroupSelect01"
+                onChange={(a) =>
+                  this.setState({ mustFieldValue: a.target.value })
+                }
+                aria-label="must"
+                value={this.state.fieldValue1}
+              >
+                <option>Select Element...</option>
+                {this.state.options.map((a) => (
+                  <option value={a}>{a}</option>
+                ))}
+              </select>
+              <button
+                onClick={this.updateMust}
+                className="btn btn-primary ml-2"
+                type="button"
+              >
+                ADD
+              </button>
+            </div>
           </div>
         </div>
       </React.Fragment>
